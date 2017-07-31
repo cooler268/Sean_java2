6ed144f883460a982d3af86c5ebac3a7983f9d79
Jonathan <cooler268@gmail.com>
2017-07-31 12:18:21 -0700

    start on Doubly Linked List

---
+++ Doubly Linked List/src/doubly/linked/list/DoublyLinkedList.java
@@ -0,0 +1,186 @@
+package doubly.linked.list;
+
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class DoublyLinkedList {
+
+    public enum position {
+
+        FIRST, PREVIOUS, FOLLOWING, LAST
+    };
+
+    private class node {
+
+        String item;
+        node prev;
+        node next;
+    }
+
+    private node first = null;
+    private node current = null;
+    private node last = null;
+    private int currentPosition = 0;
+
+    public void setPosition(position pos) {
+        switch (pos) {
+            case FIRST:
+                current = first;
+                currentPosition = 0;
+                break;
+
+            case PREVIOUS:
+                if (current == first) {
+                    System.out.println("Position is already first");
+                } else {
+                    current = current.prev;
+                    currentPosition--;
+                }
+                break;
+                
+
+            case FOLLOWING:
+                if (current == last) {
+                    System.out.println("Position is already last");
+                } else {
+                    current = current.next;
+                    currentPosition++;
+                }
+                break;
+                
+
+            case LAST:
+                current = first;
+                int lastIndex = 0;
+                while (true) {
+                    if (current.next == null) {
+                        break;
+                    }
+                    current = current.next;
+                    lastIndex++;
+                }
+                current = last;
+                currentPosition = lastIndex;
+                break;
+
+        }
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    public String getItem() throws NoSuchElementException {
+        if (current == null) {
+            throw new NoSuchElementException();
+        } else {
+            return current.item;
+        }
+    }
+
+    public int getPosition() throws NoSuchElementException {
+        if (first == null) {
+            throw new NoSuchElementException();
+        } else {
+            return currentPosition;
+        }
+    }
+
+    public void delete() throws NoSuchElementException {
+        if (first == null) {
+            last = null;
+            current = null;
+            throw new NoSuchElementException();
+        } else if (current == null) {
+            throw new NoSuchElementException();
+        } else {
+            if (current == first) {
+                first = first.next;
+                first.prev = null;
+                current = first;
+            } else if (current == last) {
+                last = last.prev;
+                last.next = null;
+                current = last;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+        }
+    }
+
+    public void insert(String item, position pos) throws IllegalArgumentException {
+        node n = new node();
+        n.item = item;
+        switch (pos) {
+            case FIRST:
+                if (first == null) {
+                    first = n;
+                    current = n;
+                    last = n;
+                } else {
+                    n.next = first;
+                    first.prev = n;
+                    n.prev = null;
+                    first = n;
+                }
+                setPosition(position.FIRST);
+                break;
+
+
+            case PREVIOUS:
+                if (current == first) {
+                    throw new IllegalArgumentException();
+                }
+
+                n.prev = current.prev;
+                n.next = current;
+                current.prev.next = n;
+                current.prev = n;
+                current = n;
+                break;
+
+            case FOLLOWING:
+                if (current == last) {
+                    throw new IllegalArgumentException();
+                }
+                n.next = current.next;
+                n.prev = current;
+                current.next.prev = n;
+                current.next = n;
+                current = n;
+                break;
+
+            case LAST:
+                if (first == null) {
+                    first = n;
+                    current = n;
+                    last = n;
+                } else {
+                    n.prev = last;
+                    last.next = n;
+                    n.next = null;
+                    last = n;
+                    setPosition(position.LAST);
+                }
+                break;
+                
+        }
+
+    }
+    
+    public void traverse() {
+        current = first;
+        while (true) {
+            if (current == null) {
+                break;
+            }
+            System.out.println(current);
+            current = current.next;
+        }
+    }
+
+}
---
+++ Doubly Linked List/src/doubly/linked/list/DoublyLinkedListTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doubly.linked.list;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class DoublyLinkedListTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DoublyLinkedList L = new DoublyLinkedList();
+        L.insert("Bob", DoublyLinkedList.position.FIRST);
+        L.traverse();
+        System.out.println(L.getPosition());
+//        L.insert("Steve", DoublyLinkedList.position.FIRST);
+//        L.traverse();
+//        System.out.println(L.getPosition());
+//        L.insert("Chelsea", DoublyLinkedList.position.FOLLOWING);
+//        L.traverse();
+//        System.out.println(L.getPosition());
+//        L.insert("book", DoublyLinkedList.position.FOLLOWING);
+//        L.traverse();
+//        System.out.println(L.getPosition());
+//        L.insert("table", DoublyLinkedList.position.LAST);
+//        L.traverse();
+//        System.out.println(L.getPosition());
+
+    }
+
+}
